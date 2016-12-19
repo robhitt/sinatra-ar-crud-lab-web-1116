19f11f60407c51c9fd3b2edaae502c734436ccbc
Rob Hitt <robhitt@gmail.com>
2016-12-18 22:11:58 -0500

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,52 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do 
-    
+  get '/' do
+
   end
+
+  get '/posts/new' do
+    erb :new
+  end
+
+
+  post '/posts' do
+    @post = Post.create(name: params["name"], content: params["content"])
+    redirect to '/posts'
+  end
+
+
+  get '/posts' do
+    @posts = Post.all
+    erb :index
+  end
+
+
+  get '/posts/:id' do
+    @post = Post.find_by_id(params[:id])
+    erb :show
+  end
+
+
+  get '/posts/:id/edit' do
+    @post = Post.find_by_id(params[:id])
+    erb :edit
+  end
+
+  patch '/posts/:id' do
+    @post = Post.find_by_id(params[:id])
+    @post.name = params[:name]
+    @post.content = params[:content]
+    @post.save
+    erb :show
+  end
+
+
+  delete '/posts/:id/delete' do
+    @deleted_post = Post.find_by_id(params[:id])
+
+    Post.delete(params[:id])
+    erb :deleted
+  end
+
 end
--- app/models/model.rb
+++ app/models/model.rb
@@ -1,1 +1,25 @@
 #Placeholder for a model
+class Post < ActiveRecord::Base
+
+  # attr_accessor :name, :content
+  #
+  # POSTS = []
+  #
+  # def initialize(posts_hash)
+  #   @name = posts_hash[:name]
+  #   @content = posts_hash[:content]
+  #   POSTS << self
+  # end
+  #
+  # def self.all
+  #   POSTS
+  # end
+  #
+  # def self.create(posts_hash)
+  #   #binding.pry
+  #   @name = posts_hash[:name]
+  #   @content = posts_hash[:content]
+  #   POSTS << self
+  # end
+  #
+end
